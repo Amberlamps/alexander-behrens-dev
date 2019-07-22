f00427320cff9b42690e5dd7f951b42ea3aacbcb
Alexander Behrens <alexander.behrens.84@gmail.com>
2019-07-22 23:10:52 +0200

    first blog post

--- src/routes/posts/2019-06-30-lazy-dependency-pattern/post.js
+++
@@ -1,6 +0,0 @@
-export default {
-  title: "Lazy Dependency Pattern",
-  tags: ['javascript', 'patterns', 'lazy'],
-  spoiler: "There are dependencies like databases connections that take some time connect. It is not necessary to establish a connection to a database when working in development on something unrelated to database. The lazy dependency pattern describes a way to only connect to dependencies when connections are actually needed.",
-  getContent: () => import('./document.mdx'),
-}
